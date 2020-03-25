827b97ce6501f08f6fa65ce7fcf9ec56f17e5f9e
Rafael Henrique Dalegrave Zottesso <rafael.zottesso@ifpr.edu.br>
2020-03-25 17:53:20 -0300

    Correções no models.

--- cadastros/models.py
+++ cadastros/models.py
@@ -2,51 +2,54 @@ from django.db import models
 
 # Create your models here.
 
+
 class Estado(models.Model):
-    sigla = models.CharField(max_length=2 unique=true)
-    mome = models.CharFiel(max_length=50)
+    sigla = models.CharField(max_length=2, unique=True)
+    mome = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.sigla + " - " + self.nome 
+        return self.sigla + " - " + self.nome
+
 
 class Cidade(models.Model):
-       nome = models .CharField(max_length=50)
-       estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+    nome = models .CharField(max_length=50)
+    estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
 
-       def __str__(self):
-           return self.nome + " - " + self.estado.sigla
-
-class Funcionario(models.Model):
-        nome = models.CharField(max_length=100)
-        cpf = models.CharField(max_length=11 unique=true)
-        funcao = models.CharField(max_length=50)
-        empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
+    def __str__(self):
+        return self.nome + " - " + self.estado.sigla
 
 
 class Empresa(models.Model):
-        razao_social = models.CharField(max_length=100)
-        cnpj = models.CharField(max_length=15 unique=true)
-        inscricao_estadual = models.CharField(max_length=45)
-        cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
-        endereço = models.CharField(max_length=100)
+    razao_social = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=15, unique=True)
+    inscricao_estadual = models.CharField(max_length=45)
+    cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
+    endereço = models.CharField(max_length=100)
 
-        def __str__(self):
-            return self
+    def __str__(self):
+        return self
+
+
+class Funcionario(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=11, unique=True)
+    funcao = models.CharField(max_length=50)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
+
 
 class Contratada(models.Model):
-        empresa = models.ForeignKey(cdade, on_delete=models.PROTECT)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
 
 class Contrante(models.Model):
-        empresa = models.ForeignKey(empresa, on_delete=models.PROTECT)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
-class Propriedade(model.Models):
-        nome_propriedade = models.CharField(max_length=45)
-        endereço = models.CharField(max_length=100)
-        area_propriedade = models.CharField(max_length=45)
-        Cidade = models.ForeignKey(cidade, on_delete=models.PROTECT)
+
+class Propriedade(models.Model):
+    nome_propriedade = models.CharField(max_length=45)
+    endereço = models.CharField(max_length=100)
+    area_propriedade = models.CharField(max_length=45)
+    cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
 
 # class Contrato(model.Models):
 #         Contratada
-
-class 
